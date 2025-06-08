1341e4769bd579ad1f30d0111fec2812d66257b3
Erin Xu <148895066+ErinXU2004@users.noreply.github.com>
2025-06-08 10:22:09 +0800

    Create sample_day.py

---
+++ examples/sample_day.py
@@ -0,0 +1,25 @@
+# CalCore Daily Behavior Script: Erin's Sample Day
+
+# Breakfast: oatmeal and eggs
+EAT 320, CARB
+EAT 120, PROTEIN
+
+# Lunch: chicken breast salad with avocado
+EAT 280, PROTEIN
+EAT 160, FAT
+
+# Afternoon workout: dance
+MOVE DANCE, 40
+
+# Dinner: roasted pumpkin and tofu
+EAT 260, CARB
+EAT 200, PROTEIN
+
+# Evening activity: walking
+MOVE WALK, 30
+
+# Query the day's caloric deficit
+QUERY DEFICIT
+
+# Query BMR (Basal Metabolic Rate)
+QUERY BMR
